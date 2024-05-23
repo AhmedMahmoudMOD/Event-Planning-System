5dcdd7a81e1fb943ccd19cf42be9dbcaba432889
your_username <abdooaiman@std.mans.edu.eg>
2024-05-23 14:06:58 +0300

    Create EventService & APIController

---
+++ Event-Planning-System/IServices/IEventService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Event_Planinng_System_DAL.Models;
+namespace Event_Planning_System.IServices
+{
+	public interface IEventService
+	{
+		public bool CreateEvent(Event newEvent);
+	}
+}
---
+++ Event-Planning-System/Services/EventService.cs
@@ -0,0 +1,26 @@
+﻿using Event_Planinng_System_DAL.Models;
+using Event_Planinng_System_DAL.Unit_Of_Work;
+using Event_Planning_System.IServices;
+using System.Security.Claims;
+namespace Event_Planning_System.Services
+{
+	public class EventService : IEventService
+	{
+		UnitOfWork unitOfWork;
+		public EventService(UnitOfWork _unitOfWork)
+		{
+			unitOfWork = _unitOfWork;
+		}
+		public bool CreateEvent(Event newEvent)
+		{
+			if (newEvent == null || newEvent.EventDate <= DateTime.Today)
+				return false;
+			newEvent.DateOfCreation = DateOnly.FromDateTime(DateTime.Today);
+			newEvent.CreatorId = 1;
+			//newEvent.CreatorId = int.Parse(ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value);
+			unitOfWork.EventRepo.Add(newEvent);
+			unitOfWork.save();
+			return true;
+		}
+	}
+}
