5ab280c34bacf0945c4a82613dd09c76cdad250f
AbdelrahmanOsama007 <143852321+AbdelrahmanOsama007@users.noreply.github.com>
2024-05-24 13:26:56 +0300

    regestration

---
+++ Event-Planning-System/IServices/Iregestration.cs
@@ -0,0 +1,11 @@
+﻿using Event_Planning_System.DTO;
+
+namespace Event_Planning_System.IServices
+{
+    public interface Iregestration
+    {
+        Task AddUserAsync(UserDto userDto, string password);
+
+
+    }
+}
---
+++ Event-Planning-System/Services/Register.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Event_Planinng_System_DAL.Models;
+using Event_Planinng_System_DAL.Unit_Of_Work;
+using Event_Planning_System.DTO;
+using Event_Planning_System.IServices;
+
+namespace Event_Planning_System.Services
+
+{
+    public class Register : Iregestration
+    {
+        readonly UnitOfWork unitOfWork;
+        readonly IMapper mapper;
+        public Register(UnitOfWork _unitofwork, IMapper _mapper)
+        {
+            unitOfWork = _unitofwork;
+            mapper = _mapper;
+            
+        }
+        async Task Iregestration.AddUserAsync(UserDto userDto, string password)
+        {
+            var user = mapper.Map<User>(userDto);
+            await unitOfWork.UserRepo.Add(user, password);
+            unitOfWork.save();
+        }
+    }
+}
