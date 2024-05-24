8cef45eedb379ffef6ef669b2a2abdb3427785b0
AhmedMahmoudMOD <ahmed.mahmoud.abdelnaby98@gmail.com>
2024-05-24 20:20:42 +0300

    modifing reg api

--- Event-Planning-System/Services/Register.cs
+++ Event-Planning-System/Services/Register.cs
@@ -13,14 +13,12 @@ namespace Event_Planning_System.Services
     {
         readonly UnitOfWork _unitOfWork;
         readonly IMapper mapper;
-        private readonly UserManager<User> _userManager;
         
 
         public Register(UnitOfWork _unitofwork, IMapper _mapper, UserManager<User> userManager)
         {
             _unitOfWork = _unitofwork;
             mapper = _mapper;
-            _userManager = userManager;
             
         }
         public async Task<IdentityResult> adduser(UserDto userDto)
@@ -28,11 +26,9 @@ namespace Event_Planning_System.Services
             // Map UserDto to User
             
             var user = mapper.Map<User>(userDto);
-            user.UserName = userDto.Email;
 
             // Create user with UserManager and set the password
-             var result =  await _unitOfWork.UserRepo.Add(user,userDto.Password) ;
-            await _unitOfWork.UserRepo.AddUserToRoles(user);
+            var result = await _unitOfWork.UserRepo.Add(user, userDto.Password);
             return result;
         }
     }
