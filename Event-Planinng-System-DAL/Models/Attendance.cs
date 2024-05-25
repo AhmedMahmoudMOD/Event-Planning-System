d7a7ae339a49dfe4a75dd56559fec101eb0605f2
abdellatief <abdellatiefhamed00@gmail.com>
2024-05-25 18:21:30 +0300

    changing attendance Model

--- Event-Planinng-System-DAL/Models/Attendance.cs
+++ Event-Planinng-System-DAL/Models/Attendance.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Event_Planinng_System_DAL.Models
 {
-    public class Attendance : InheritIdAndIsDeleted
+    public class Attendance 
     {
+        [ForeignKey("EventNavigation")]
+        public int EventId {  get; set; }
         [EmailAddress(ErrorMessage = "invalid email message")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         public bool IsSent { get; set; }
+
 
         public virtual Event EventNavigation { get; set; }
     }
