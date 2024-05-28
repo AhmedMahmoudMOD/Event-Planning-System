a564f24fcfdc41485a2109ed9abce3ad5798cfbe
AbdelrahmanOsama007 <143852321+AbdelrahmanOsama007@users.noreply.github.com>
2024-05-28 21:14:15 +0300

    side bar after modification design

--- Event-Planning-System-Angular/src/app/sidebar/sidebar.component.ts
+++ Event-Planning-System-Angular/src/app/sidebar/sidebar.component.ts
@@ -8,12 +8,12 @@ import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-sidebar',
+  
   standalone: true,
   imports: [SidebarModule,FormsModule,ButtonModule,AvatarModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
-  sidebarVisible :boolean = true;
-  
+export class SidebarComponent {                          
+  sidebarVisible :boolean = false;
 }
