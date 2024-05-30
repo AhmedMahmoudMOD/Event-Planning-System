a0f86c113d89e1a4711603c0ab6f6731aee8c2d0
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-05-30 18:38:13 +0300

    Event List Component

--- Event-Planning-System-Angular/src/app/events/event-list/event-list.component.ts
+++ Event-Planning-System-Angular/src/app/events/event-list/event-list.component.ts
@@ -1,36 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component , OnInit } from '@angular/core';
-import { Event , EventListRes } from "../../shared/models/eventsListRes.model";
+import { Event, EventType } from "../../shared/models/eventsListRes.model";
 import { EventService } from "../../shared/services/event.service";
 import { TableLazyLoadEvent, TableModule } from 'primeng/table';
 import { LazyLoadEvent } from 'primeng/api';
+import { RouterLink, RouterModule } from '@angular/router';
+import { PaginatorModule } from 'primeng/paginator';
+import { EventListService } from '../../shared/services/event-list.service';
 
+interface PageEvent {
+  first: number;
+  rows: number;
+  page: number;
+  pageCount: number;
+}
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [CommonModule,TableModule],
+  imports: [CommonModule,TableModule, RouterModule, RouterLink, PaginatorModule],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
-export class EventListComponent{
+export class EventListComponent implements OnInit{
   eventList: Event[]  = [];
-  total : number = 0;
-  loading : boolean = false;
+  constructor(private eventListService: EventListService) {}
 
-  constructor(private eventService: EventService) {}
-
-  loadEvents(event:TableLazyLoadEvent) {
-    this.loading = true;
-    console.log(event);
-    let pageNum = event.first! / event.rows! + 1;
-    // Example: Accessing a specific filter value
-    this.eventService.getEvents(pageNum,event.rows??3,"").subscribe((res: EventListRes) => {
-      console.log(res);
-      this.loading = false;
-      this.eventList = res.events;
-      this.total = res.totalCount;
-    });
+  ngOnInit(): void { 
+    this.eventListService.getAll(1).subscribe({next: d=>{
+      this.eventList = d;
+    }});
   }
 
+  getEventTypeLabel(type: EventType): string {
+        return EventType[type]; // Convert enum value to string
+    }
 }
