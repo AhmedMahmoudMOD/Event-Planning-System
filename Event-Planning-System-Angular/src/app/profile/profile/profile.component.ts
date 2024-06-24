1c226536be6de6629f30e5823a18a9bf2c94adca
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-06-24 19:48:49 +0300

    create update profile component

---
+++ Event-Planning-System-Angular/src/app/profile/profile/profile.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProfileService } from '../../shared/services/profile.service';
+import { Profile } from '../../shared/models/profile';
+import { AccountService } from '../../shared/services/account.service';
+import { finalize } from 'rxjs';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule, RouterModule, RouterLink, FormsModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class ProfileComponent implements OnInit {
+  title = 'Profile';
+  profile: Profile | any = {};
+  defaultImage = 'assets/images/default_Image.jpg';
+
+  eventList: Event[] = [];
+  // count the number of events in eventList array
+  eventCount = this.eventList.length;
+
+  isLoading: boolean = false;
+
+  constructor(
+    private profileService: ProfileService,
+    private accountService: AccountService,
+    private route: ActivatedRoute
+  ) {}
+  //get profile by user id
+  ngOnInit(): void {
+    const id = +this.accountService.extractUserID();
+    this.isLoading = true;
+    this.profileService
+      .getProfile(id)
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe({
+        next: (d) => {
+          this.profile = d;
+        },
+      });
+  }
+
+  // update profile
+  updateProfile(): void {
+    this.profileService.updateProfile(this.profile).subscribe({
+      next: (d) => {
+        this.profile = d;
+      },
+    });
+  }
+}
